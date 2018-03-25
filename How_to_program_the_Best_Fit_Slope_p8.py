b24d1060ffb653d87af25d98a888be0f8fd9117c
jiaqi <jiaqiperson@hotmail.com>
2018-03-25 18:26:43 -0500

    video 11

--- How_to_program_the_Best_Fit_Slope_p8.py
+++ How_to_program_the_Best_Fit_Slope_p8.py
@@ -2,8 +2,11 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 
-xs=np.array([1,2,3,4,5,6],dtype=np.float64)
-ys=np.array([5,4,6,5,6,7],dtype=np.float64)
+# xs=np.array([1,2,3,4,5,6],dtype=np.float64)
+# ys=np.array([5,4,6,5,6,7],dtype=np.float64)
+
+xs=np.array([1,2,3,4,5,6])
+ys=np.array([5,4,6,5,6,7])
 
 def best_fit_slope(xs,ys):
     m=(((mean(xs)*mean(ys))-mean(xs*ys))
--- Programming_R_Squared_p11.py
+++ Programming_R_Squared_p11.py
@@ -5,9 +5,6 @@ from matplotlib import style
 
 style.use('fivethirtyeight')
 
-# xs=np.array([1,2,3,4,5,6],dtype=np.float64)
-# ys=np.array([5,4,6,5,6,7],dtype=np.float64)
-
 xs=np.array([1,2,3,4,5,6])
 ys=np.array([5,4,6,5,6,7])
 
@@ -20,6 +17,15 @@ def best_fit_slope_and_intercept(xs,ys):
 m,b=best_fit_slope_and_intercept(xs,ys)
 #print(m,b)
 
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line-ys_orig)**2)
+
+def coefficient_of_determination(ys_orig,ys_line):
+    y_mean_line=[mean(ys_orig) for y in ys_orig]
+    squared_error_regr=squared_error(ys_orig,ys_line)
+    squared_error_y_mean=squared_error(ys_orig,y_mean_line)
+    return 1-(squared_error_regr/squared_error_y_mean)
+
 
 regression_line=[(m*x)+b for x in xs] 
 predict_x=8
@@ -28,6 +34,8 @@ predict_y=m*predict_x+b
 # for x in xs:
 #     regression_line=m*x+b
 
+r_squared=coefficient_of_determination(ys,regression_line)
+print(r_squared)
 plt.scatter(xs,ys)
 plt.scatter(predict_x,predict_y,color='g')
 plt.plot(xs,regression_line)
